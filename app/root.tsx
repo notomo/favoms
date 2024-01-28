8078b45581530973650b1dc9f8db47157a5b3d97
notomo <notomo.motono@gmail.com>
2024-01-28 17:19:18 +0900

    Enable dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   return (
-    <html lang="en" className="flex w-full h-full bg-slate-800 text-white">
+    <html lang="en" className="dark flex w-full h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
