eeb919abb46af8fea5b0bd51160dc344bc7c4893
notomo <notomo.motono@gmail.com>
2024-01-30 09:47:39 +0900

    Adjust home

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,19 +3,15 @@ import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "favoms" },
+    { name: "description", content: "Welcome to favoms!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="text-3xl font-bold underline">
-      <ul>
-        <li>
-          <Link to="mylist">Mylist</Link>
-        </li>
-      </ul>
+    <div className="w-full h-full flex flex-col items-center justify-center text-3xl font-bold">
+      <Link to="mylist">Mylist</Link>
     </div>
   );
 }
