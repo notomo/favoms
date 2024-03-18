1cf399804d680bfb868b364b102baf4aee8364d7
notomo <notomo.motono@gmail.com>
2024-03-19 08:45:01 +0900

    Add book.author._index route to show empty frame

---
+++ app/routes/book.author._index/route.tsx
@@ -0,0 +1,12 @@
+import { getBookAuthors } from "~/routes/book.author/loader";
+
+export const loader = getBookAuthors;
+
+export default function Page() {
+  return (
+    <div className="grid h-full w-full grid-cols-2 grid-rows-[100%] gap-x-4">
+      <div className="h-full w-full border"></div>
+      <div className="h-full w-full border"></div>
+    </div>
+  );
+}
